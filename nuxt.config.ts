255368e55676595cbe6701b928166ad3c012eb7b
KhurshidShod <khurshid.lenovo@gmail.com>
2025-04-04 13:42:37 +0500

    asd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  app: {
+    head: {
+      link: [
+        {rel: "icon", type: "image/svg+xml", href: "/favicon.svg"}
+      ]
+    }
+  },
   devtools: { enabled: true },
   css: ["~/assets/styles/main.scss"],
   vite: {
